9f8ea7633d502812fe408eb42a357eafc078383e
MrRobot420 <max.karlchen@web.de>
2021-12-31 14:53:32 +0100

    implement bot logic

--- trade_bot/bot.js
+++ trade_bot/bot.js
@@ -7,21 +7,20 @@
 
 
 class Bot {
-    constructor(pos, dna, totalRockets) {
+    constructor(pos, dna) {
       // All of our physics stuff
       this.position = pos.copy();
-      this.r = 4;
       this.dna = dna;
   
       this.fitness = 0;
       this.geneCounter = 0;
+      this.recordAmount = 100; // Some low number that will be beat instantly
+      this.startCapital = 2000;
+      this.currentCapital = this.startCapital;
+      this.exceededStartCapital = false;
     }
   
     // FITNESS FUNCTION
-    // distance = distance from target
-    // finish = what order did i finish (first, second, etc. . .)
-    // f(distance,finish) =   (1.0f / finish^1.5) * (1.0f / distance^6);
-    // a lower finish is rewarded (exponentially) and/or shorter distance to target (exponetially)
     calcFitness() {
       if (this.recordDist < 1) this.recordDist = 1;
   
@@ -31,45 +30,44 @@ class Bot {
       // Make the function exponential
       this.fitness = pow(this.fitness, 6);
   
-      if (this.hitObstacle) this.fitness *= 0.1; // lose 90% of fitness hitting an obstacle
-      if (this.hitTarget) this.fitness *= 3; // twice the fitness for finishing!
+      if (this.currentCapital <= 0) this.fitness *= 0.1; // lose 90% of fitness hitting an obstacle
+      if (this.exceededStartCapital) this.fitness *= 2; // twice the fitness for finishing!
     }
   
     // Run in relation to all the obstacles
     // If I'm stuck, don't bother updating or checking for intersection
-    run(os) {
-      if (!this.hitObstacle && !this.hitTarget && !this.outOfBounds) {
-        this.applyForce(this.dna.genes[this.geneCounter]);
+    run(course) {
+      if (this.currentCapital !== 0) {
+        this.executeAction(this.dna.genes[this.geneCounter], course);
         this.geneCounter = (this.geneCounter + 1) % this.dna.genes.length;
-        this.update();
-        // If I hit an edge or an obstacle
-        this.obstacles(os);
       }
       // Draw me!
-      if (!this.hitObstacle && !this.outOfBounds) {
-        this.display();
-      }
+      this.display();  
+    }
+
+    executeAction(action, course) {
+        switch (action) {
+            case 'B':
+                if (this.currentCapital >= course) {
+                    this.currentCapital -= course;
+                }
+                break;
+            case 'S':
+                this.currentCapital += course;
+                break;
+            case 'H':
+                break;
+        }
     }
   
-    // Did I make it to the target?
+    // Did I earn a good amount of money?
     checkTarget() {
-      let d = dist(this.position.x, this.position.y, target.position.x, target.position.y);
-      if (d < this.recordDist) this.recordDist = d;
-  
-      if (target.contains(this.position) && !this.hitTarget) {
-        this.hitTarget = true;
-      } else if (!this.hitTarget) {
-        this.finishTime++;
-      }
-    }
-  
-    update() {
-      
+      if (this.recordAmount < this.currentCapital) this.recordAmount = this.currentCapital;
+      if (this.startCapital < this.recordAmount) this.exceededStartCapital = true;
     }
   
     display() {
-      
-  
+      text(`Captial: ${this.currentCapital} €, Last Action: ${this.currentCapital}`, this.position.x, this.position.y);
       pop();
     }
   
